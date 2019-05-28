e75fc136f9dc17f32277f5b4645f363b935e5704
keithtoots <keith@smartblock.pro>
2019-05-28 17:02:19 +0800

    commit now

---
+++ src/Providers/WebsiteServiceProvider.php
@@ -0,0 +1,17 @@
+<?php 
+
+namespace Desmond\Website\Providers;
+use Illuminate\Support\ServiceProvider;
+
+
+class WebsiteServiceProvider extends ServiceProvider{
+    public function boot(){
+    	dd("nbiibbibibi");
+    }
+
+    public function register(){
+
+    }
+
+
+}## [Class]
